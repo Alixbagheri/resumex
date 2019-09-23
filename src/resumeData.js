81dc8f565494af7e9b0f6346926f5274b4374dc6
AliX7M <bagheriali29101380@gmail.com>
2019-09-23 16:30:40 +0330

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,42 @@
+let resumeData = {
+    "imagebaseurl":"#",
+    "name": "Ali Bagheri",
+    "role": "Frontend Developer and Graphic Designer",
+    "linkedinId":"https://www.linkedin.com/",
+    "skypeid": "#",
+    "roleDescription": "i love learning new things - travel and many more",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"http://www.linkedin.com/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"i'm a student in iran - shahr-e kord and i study SoftWare Engineering - i love coding, illustration and graphics - i can create greate UI - UX design",
+    "address":"iran",
+    "website":"https://hi-t.ir",
+    "skillsDescription":"i have this Skills:",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+  }
+  
+  export default resumeData
