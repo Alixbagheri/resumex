d4c0065fadfbe274320ee218cc4f6f066ac050e4
AliX7M <bagheriali29101380@gmail.com>
2019-09-23 17:10:57 +0330

    Updates

---
+++ precache-manifest.26d7f1885ce6bf6bba0707f3d478af99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "40dafb444062aeda8a52",
+    "url": "/resumex/static/css/main.829edd44.chunk.css"
+  },
+  {
+    "revision": "40dafb444062aeda8a52",
+    "url": "/resumex/static/js/main.40dafb44.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/resumex/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "af478fd1fb0529253322",
+    "url": "/resumex/static/js/runtime~main.af478fd1.js"
+  },
+  {
+    "revision": "e65e29fb89fc7fb4a36678ed840b7ca1",
+    "url": "/resumex/index.html"
+  }
+];
